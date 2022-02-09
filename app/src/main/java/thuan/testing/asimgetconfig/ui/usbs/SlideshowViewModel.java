f7b6bc2fa26ef679ad8a7d288d95d03ddc7e4b4f
thuanpv1 <thuanpv@egt.vn>
2022-02-09 18:23:04 +0700

    update configuration data

--- app/src/main/java/thuan/testing/asimgetconfig/ui/usbs/SlideshowViewModel.java
+++ app/src/main/java/thuan/testing/asimgetconfig/ui/usbs/SlideshowViewModel.java
@@ -1,6 +1,7 @@
 package thuan.testing.asimgetconfig.ui.usbs;
 
 import android.content.Context;
+import android.os.Build;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
@@ -23,7 +24,20 @@ public class SlideshowViewModel extends ViewModel {
         List<DeviceInfo> devices2 = SDKLocker.getListUsbDevices(context);
         String result = "Danh sách các cổng usb - thư viện SDKLocker: usb-to-com \n";
         for (DeviceInfo devi: devices2) {
-            result += devi.device.getDeviceName()  + ", productId=" + devi.device.getProductId() + ", vendorId=" + devi.device.getVendorId()+ ", having usb-to-com driver: " + (devi.driver != null ? "yes" : "no") + "\n\n";
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    result += "getDeviceName=" + devi.device.getDeviceName()
+                            + "| getProductName=" + devi.device.getProductName()
+                            + "| getSerialNumber=" + devi.device.getSerialNumber()
+                            + "| getDeviceId=" + devi.device.getDeviceId()
+                            + "| getVersion=" + devi.device.getVersion()
+                            + "| getDeviceProtocol=" + devi.device.getDeviceProtocol()
+                            + "| getManufacturerName=" + devi.device.getManufacturerName()
+                            + "| productId=" + devi.device.getProductId()
+                            + "| vendorId=" + devi.device.getVendorId()
+                            + "| having usb-to-com driver: " + (devi.driver != null ? "yes" : "no") + "\n\n\n\n";
+                }
+            }
         }
 
         return result;
